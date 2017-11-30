432529f4fcd5ab6c21095247f73cf6b27826e4b0
bgrochal <bartek.grochal@gmail.com>
2017-11-30 23:34:17 +0100

    Integration with the Configuration Server.

--- src/main/java/pl/edu/agh/iet/dts/discovery/ServiceDiscovery.java
+++ src/main/java/pl/edu/agh/iet/dts/discovery/ServiceDiscovery.java
@@ -1,12 +1,16 @@
 package pl.edu.agh.iet.dts.discovery;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author Bartłomiej Grochal
  */
+@SpringBootApplication
 public class ServiceDiscovery {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(ServiceDiscovery.class, args);
     }
 
 }
