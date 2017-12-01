6c1c778e0b4d2d0c3eddc7f517d6a45fba479aa6
bgrochal <bartek.grochal@gmail.com>
2017-12-01 17:41:57 +0100

    Implementation of the Eureka service discovery added.

--- src/main/java/pl/edu/agh/iet/dts/discovery/ServiceDiscovery.java
+++ src/main/java/pl/edu/agh/iet/dts/discovery/ServiceDiscovery.java
@@ -2,11 +2,13 @@ package pl.edu.agh.iet.dts.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author Bartłomiej Grochal
  */
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceDiscovery {
 
     public static void main(String[] args) {
